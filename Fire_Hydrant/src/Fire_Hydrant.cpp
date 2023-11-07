e4a3f1c95007a8491f4486db3501117478fcb427
unknown <alazar@cnm.edu>
2023-11-07 15:52:13 -0700

    All sensors connected, need to debug

---
+++ Fire_Hydrant/src/Fire_Hydrant.cpp
@@ -0,0 +1,170 @@
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+
+#include "Particle.h"
+#include "Air_quality_Sensor.h"
+#include <Adafruit_MQTT.h>
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "Credentials.h"
+#include <math.h>
+#include "Adafruit_SSD1306.h"
+#include "Adafruit_BME280.h"
+SYSTEM_MODE(AUTOMATIC);
+TCPClient TheClient;
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+Adafruit_MQTT_Publish airQualFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/airQualityFeed");
+Adafruit_MQTT_Publish dustFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/dustfeed");
+const int OLED_RESET = -1, PUMP= D16;
+const char hexAddress = 0x76;
+Adafruit_SSD1306 display(OLED_RESET);
+AirQualitySensor airQualSens(A1);
+Adafruit_BME280 bme;
+int status,tempC,pressPA,humidRH,tempF,inHG, soilMoist = A2,lastTime;
+const int DUSTSENS = A0;
+unsigned int startTime1, startTime2,startTime3 , airValue, duration, lowPulseOcc, currentQuality = -1;
+float ratio = 0, concentration = 0;
+void bmeReading();
+void dustSensor();
+void MQTT_connect();
+void airSens();
+void readSoil();
+
+void setup() {
+  WiFi.on();
+  WiFi.connect();
+  Serial.begin(9600);
+  waitFor(Serial.isConnected,10000);
+  pinMode(soilMoist,INPUT);
+  status = bme.begin(hexAddress);
+  if(status == false){
+    Serial.printf("BME280 at address 0x%02X failed to start", hexAddress);
+  }
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(2000); 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  startTime1 = millis();
+  startTime2 = millis();
+  startTime3 = millis();
+  pinMode(PUMP,OUTPUT);
+}
+
+void loop() {
+  MQTT_connect();
+  bmeReading();
+  airSens();
+  if((millis()-startTime3)>6000){
+  readSoil();
+  startTime3 = millis();
+  }
+  dustSensor();
+}
+
+
+void bmeReading(){
+  display.clearDisplay();
+  tempC= bme.readTemperature();
+  pressPA = bme.readPressure();
+  humidRH = bme.readHumidity();
+
+  tempF = map(tempC,0,38,32,100);
+  inHG = map(pressPA,0,135456,0,40);
+
+  display.setCursor(0,0);
+  display.printf("Temp F: %i",tempF);
+  display.display();
+  display.setCursor(0,20);
+  display.printf("Pressure: %i",inHG);
+  display.display();display.setCursor(0,40);
+  display.printf("Humidity: %i",humidRH);
+  display.display();
+  return;
+}
+
+
+void dustSensor(){  //Reads air particles in room and prints/publishs the data 
+  duration = pulseIn(DUSTSENS,LOW);
+  lowPulseOcc = lowPulseOcc+duration;
+
+  if((millis()-startTime1)>30000){
+    ratio = lowPulseOcc/(300000.0);
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,3)+520*ratio+0.62;
+    Serial.printf("Low Pulse Occ = %i, Ratio = %f, Concentration = %f\n",lowPulseOcc,ratio,concentration);
+    //dustFeed.publish(concentration);
+    lowPulseOcc = 0;
+    startTime1 = millis();
+  }
+  return;
+}
+
+void airSens(){  //reads air qualuity and gives back a reading in case form asell as quantitative value 
+  if((millis()-startTime2)>500){
+  airValue = airQualSens.getValue();
+  currentQuality = airQualSens.slope();
+  switch(currentQuality){
+    case 0:
+      Serial.printf("HIGH POLLUTION, YOURE GOING TO DIE!\n");
+      //airQualFeed.publish("HIGH POLLUTION, YOURE GOING TO DIE!\n");
+      break;
+    case 1:
+      Serial.printf("High pollution!\n");
+      //airQualFeed.publish("High pollution!\n");
+      break;
+    case 2:
+      Serial.printf("Low polution.\n");
+      //airQualFeed.publish("Low polution.\n");
+      break;
+    case 3:
+      Serial.printf("Fresh Air.\n");
+      //airQualFeed.publish("Fresh Air.\n");
+      break;
+  }
+  Serial.printf("Quant Value= %i\n",airValue);
+  startTime2 = millis();
+}
+return;
+}
+
+
+void readSoil(){ // checks moisture of soil and waters if neccesary 
+  int moistReading = analogRead(soilMoist);
+    if(moistReading < 2700){
+      /* digitalWrite(PUMP,HIGH);
+      delay(500);
+      digitalWrite(PUMP,LOW); */
+      Serial.printf("Soil dry, watering now\n");
+    }
+    else{
+      Serial.printf("Soil good");
+    }
+    return;
+}
+
+
+void MQTT_connect() {
+  int8_t ret;
+ 
+  // Return if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
+}
